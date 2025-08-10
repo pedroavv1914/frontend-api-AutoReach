b69fddd3c5c734e328a5f8a079c2885ede3769ca
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-10 18:58:53 -0300

    feat: add post management UI with composer and listing pages

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { PostComposer } from "@/components/post-composer";
-import { AuthGuard } from "@/components/auth-guard";
 
 export default function NewPostPage() {
   return (
-    <AuthGuard>
-      <div className="p-6">
-        <PostComposer />
-      </div>
-    </AuthGuard>
+    <div className="p-6">
+      <PostComposer />
+    </div>
   );
 }
